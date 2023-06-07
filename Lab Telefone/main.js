8ae5131fced9aa34ab7cbb023986580b04e6348a
Rafael_Tamaki <102268927+RafaelTamaki@users.noreply.github.com>
2023-06-06 22:19:07 -0300

    Lab Telefone

--- Lab Telefone/main.js
+++ Lab Telefone/main.js
@@ -1,1 +1,44 @@
+function valorPrimeiroBotao(){
+    alert(document.querySelector('input[type=button]').value);
+ }
  
+ const listaTeclas = document.querySelectorAll('input[type=button]');
+ const display = document.querySelector('input[type=tel]');
+ 
+ function valorBotaoUmJanela(){
+    alert(listaTeclas[0].value);
+ }
+ 
+ function valorBotaoUm(){
+    display.value += listaTeclas[0].value;
+ }
+ 
+ function valorBotao(valor){
+   display.value += listaTeclas[valor].value;
+ }
+ 
+ listaTeclas[0].onclick = function(){
+    valorBotao(0);
+ }
+ /*
+ let i = 0;
+ while(i < listaTeclas.length){
+    const tecla = listaTeclas[i];
+    const indice = i;
+    tecla.onclick = function(){
+       valorBotao(indice);
+    }
+    i += 1;
+ }*/
+ 
+ for (let index = 0; index < listaTeclas.length; index++) {
+    const tecla = listaTeclas[index];
+    tecla.onclick = function() {
+       valorBotao(index);
+    }
+    
+ }
+ 
+ function limpar(){
+    display.value = '';
+ }
